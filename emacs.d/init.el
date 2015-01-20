2932e3ba6fcd63f52765de2f7a75611fe1ef3264
Mason Donahue <masond+github@gmail.com>
2015-01-19 21:57:32 -0600

    And yet further reworking.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,16 +1,27 @@
+; general settings
 (tool-bar-mode nil)
 (setq vc-follow-symlinks t)
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 
+; packagey stuff
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(setq my-package-list '(ack cider company ido magit rainbow-delimiters
+(setq my-package-list '(ack
+			anaconda-mode
+			cider
+			company
+			git-gutter+
+			ido
+			magit
+			rainbow-delimiters
 			))
+
 (mapc 'package-install my-package-list)
 
 (add-hook 'after-init-hook 'my-after-init-hook)
@@ -18,10 +29,9 @@
   (require 'ack)
   (require 'cider)
   (require 'ido)
+  ; package specific configuration
   (ido-mode t)
-  (rainbow-delimiters-mode t)
+  (global-git-gutter+-mode t)
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'after-init-hook 'global-company-mode)
   )
-
-
-(add-hook 'after-init-hook 'global-company-mode)
-
